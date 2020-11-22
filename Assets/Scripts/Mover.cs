8db6b304fa2763249ccafdd49f3bf089a57cd604
VIVEK SINGH BHANDARI <ervivekbhandari@gmail.com>
2020-11-22 18:04:04 +0530

    1. Player and Target added. 2. NavMeshAgent and NavMeshObstacle component added to player and Box(GameObject) respectively. 3. Simple Auto AI movement was added to the player, moving towards the target(GameObject) specify.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+}
