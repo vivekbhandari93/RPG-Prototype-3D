98372a5354a62616cf6b724904dbfc09a2290d9f
VIVEK SINGH BHANDARI <ervivekbhandari@gmail.com>
2020-11-27 19:04:49 +0530

    1. Started the Timeline for cinemachine on TrigggerEnter by player. 2. Also disabled the player control at a tme of cinemachine playing.

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+using RPG.Core;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+
+        private void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+            Debug.Log("COntrol are disable.");
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<PlayerController>().enabled = true;
+            Debug.Log("control are back online.");
+        }
+    }
+}
