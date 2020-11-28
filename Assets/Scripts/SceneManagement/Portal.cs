0149ac4f307f34725f8b1e3487f5da1dd241493f
VIVEK SINGH BHANDARI <ervivekbhandari@gmail.com>
2020-11-28 14:02:58 +0530

    1. Create the portals that are connected to each others, 2. Portal A from both scenes are interconnected and Portal B also connected to Portal B in another scene.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +8,12 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier { A, B, C, D, E}
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint = null;
+        [SerializeField] DestinationIdentifier destination;
+
 
 
         private void OnTriggerEnter(Collider other)
@@ -22,8 +28,30 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded.");
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) { continue; }
+                if(portal.destination != destination) { continue; }
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+
         }
     }
 }
